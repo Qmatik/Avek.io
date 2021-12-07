1067111a2ba4c14fa42c76cff8515aaae7cfaf3a
Andrew Rudyi <a.rudyi@sendpulse.com>
2021-12-07 22:41:06 +0200

    Refactor routes loading

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,9 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . 'environment.php';
-require_once __DIR__ . 'container.php';
-require_once __DIR__ . '/../routes/api.php';
+require_once __DIR__ . '/environment.php';
+require_once __DIR__ . '/container.php';
+require_once __DIR__ . '/routes.php';
 
 /** @var Laravel\Lumen\Application $app */
 return $app;
---
+++ bootstrap/routes.php
@@ -0,0 +1,6 @@
+<?php
+
+/** @var Laravel\Lumen\Application $app */
+$router = $app->router;
+
+require_once __DIR__ . '/../routes/api.php';
