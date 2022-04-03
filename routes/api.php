77b1da2ef69f97724b73f90525105df9fd57cd32
Andrew Rudyi <a.rudyi@sendpulse.com>
2022-04-03 09:22:01 +0300

    Add new routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,22 @@
 <?php
 declare(strict_types=1);
 
-use Laravel\Lumen\Routing\Router;
+use App\Http\Controllers\CitiesController;
+use App\Http\Controllers\DisabledProductsController;
+use App\Http\Controllers\ProductCategoriesController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\TradingNetworksController;
+use App\Http\Controllers\TradingPointSchedulesController;
+use App\Http\Controllers\TradingPointsController;
+use App\Http\Controllers\UsersController;
+use App\Services\Decorators\Router;
 
 /** @var Router $router */
-$router->get('/', fn () => $router->app->version());
+$router->resource('cities', CitiesController::class);
+$router->resource('disabled-products', DisabledProductsController::class);
+$router->resource('product-categories', ProductCategoriesController::class);
+$router->resource('products', ProductsController::class);
+$router->resource('trading-networks', TradingNetworksController::class);
+$router->resource('trading-point-schedules', TradingPointSchedulesController::class);
+$router->resource('trading-points', TradingPointsController::class);
+$router->resource('users', UsersController::class);
